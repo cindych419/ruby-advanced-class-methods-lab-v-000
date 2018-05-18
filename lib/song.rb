17265187fcb4a067e97db929d2e8364c40be06bb
cindych419@gmail.com <cindych419@gmail.com>
2018-05-18 01:54:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,26 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+   song = Song.new
+   song.save
+   song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = Song.new
+    song.name = name
+    song.save
+    song
+  end
+
+
+
+
 end
